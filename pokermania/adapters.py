54fdca22bb055e3847bec81a5b08bdb4c5bb3cf1
prasuk-jain67 <prasuk0129@gmail.com>
2026-01-31 18:39:42 +0530

    duplicate user name error

--- pokermania/adapters.py
+++ pokermania/adapters.py
@@ -3,21 +3,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def populate_user(self, request, sociallogin, data):
-        """
-        Populate required fields for custom User model
-        """
-        user = sociallogin.user
+        
+        user = super().populate_user(request, sociallogin, data)
 
-        email = data.get("email", "")
-        name = data.get("name", "")
-
-        # fallback username if required
-        if not getattr(user, "username", None):
-            user.username = email.split("@")[0]
-
-        user.email = email
-
+        
+        user.email = data.get("email")
         return user
